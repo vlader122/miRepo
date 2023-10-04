8802ee4cbe0fe7b8d1bcf9e03e1794aedfa43cfe
Niver Copa <vlader122@gmail.com>
2023-10-04 15:45:39 -0400

    mis primeros cambios

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  // La variable data es la que va a almacenar los datos de "stays.json" y setData nos ayudará a guardar esos datos en esa variable. Es necesario que inicialicemos esa variable como un array vacío para evitar errores.
+  const [data, setData] = useState([]);
+
+  // Función para traer los datos de "stays.json".
+  const getData = async () => {
+    // Esta sentencia try-catch sirve para manejar los errores que se podrían generar al importar los datos de "stays.json".
+    try {
+      const res = await fetch("stays.json");
+      const resJson = await res.json();
+      // Aquí guardamos los datos de "stays.json" en la variable data.
+      setData(resJson);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Este Hook te va a ejecutar la función getData cada vez que la página se renderice.
+  useEffect(() => {
+     getData();
+  }, []);
+
+  // Puedes ver la variable data en consola.
+  console.log(data);
+  return (
+    <>
+    <div className="container">
+      <div className="row">
+      {data.map((el, i) => {
+          if(el.superHost){
+
+          }
+          return <div className="col-4">
+            <h1 key={i}>{el.city}</h1>
+            <img src={el.photo} width={450} height={300} className="p-2 m-2"></img>
+          </div>;
+        })}
+    </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
